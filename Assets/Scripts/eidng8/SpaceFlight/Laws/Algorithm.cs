3f0c77e2bc84b6ef830f412f701bc2be4602514b
eidng8 <cheung.jackey@gmail.com>
2020-01-21 22:11:35 +0800

    create algorithm file

---
+++ Assets/Scripts/eidng8/SpaceFlight/Laws/Algorithm.cs
@@ -0,0 +1,66 @@
+﻿// ---------------------------------------------------------------------------
+// <copyright file="Maths.cs" company="eidng8">
+//      GPLv3
+// </copyright>
+// <summary>
+// 
+// </summary>
+// ---------------------------------------------------------------------------
+
+using System;
+using eidng8.SpaceFlight.Components;
+using eidng8.SpaceFlight.Managers;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace eidng8.SpaceFlight.Laws
+{
+    public static class NativeArrayExtensions
+    {
+        public static int BinarySearch<T>(this NativeArray<T> array, T search)
+            where T : struct, IComparable<T> {
+            int lower = 0;
+            int upper = array.Length - 1;
+            if (0 == upper) {
+                if (array[0].Equals(search)) { return 0; }
+
+                return -1;
+            }
+
+            // Rider suggests to use loop instead of a tail recursion,
+            // which is reasonably well. Here we go.
+            while (true) {
+                if (upper < lower) {
+                    // We reach here when element is not present in array 
+                    return -1;
+                }
+
+                int mid = lower + (upper - lower) / 2;
+
+                // If the element is present at the 
+                // middle itself 
+                if (array[mid].Equals(search)) { return mid; }
+
+                // If element is smaller than mid, then 
+                // it can only be present in lower sub-array 
+                if (array[mid].CompareTo(search) < 0) {
+                    upper = mid - 1;
+                    continue;
+                }
+
+                // Else the element can only be present 
+                // in upper sub-array 
+                lower = mid + 1;
+            }
+        }
+
+        public static int BinarySearch(
+            this NativeArray<PrefabComponent> array,
+            int type
+        ) {
+            return array.BinarySearch(
+                new PrefabComponent((PrefabTypes)type, Entity.Null)
+            );
+        }
+    }
+}
--- Assets/Scripts/eidng8/SpaceFlight/Laws/Maths.cs
+++ Assets/Scripts/eidng8/SpaceFlight/Laws/Maths.cs
@@ -8,10 +8,6 @@
 // ---------------------------------------------------------------------------
 
 using System;
-using eidng8.SpaceFlight.Components;
-using eidng8.SpaceFlight.Managers;
-using Unity.Collections;
-using Unity.Entities;
 using UnityEngine;
 
 namespace eidng8.SpaceFlight.Laws
@@ -216,56 +212,6 @@ namespace eidng8.SpaceFlight.Laws
 
         public static Vector3 InverseScale(this Vector3 a, Vector3 b) {
             return new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
-        }
-    }
-
-
-    public static class NativeArrayExtensions
-    {
-        public static int BinarySearch<T>(this NativeArray<T> array, T search)
-            where T : struct, IComparable<T> {
-            int lower = 0;
-            int upper = array.Length - 1;
-            if (0 == upper) {
-                if (array[0].Equals(search)) { return 0; }
-
-                return -1;
-            }
-
-            // Rider suggests to use loop instead of a tail recursion,
-            // which is reasonably well. Here we go.
-            while (true) {
-                if (upper < lower) {
-                    // We reach here when element is not present in array 
-                    return -1;
-                }
-
-                int mid = lower + (upper - lower) / 2;
-
-                // If the element is present at the 
-                // middle itself 
-                if (array[mid].Equals(search)) { return mid; }
-
-                // If element is smaller than mid, then 
-                // it can only be present in lower sub-array 
-                if (array[mid].CompareTo(search) < 0) {
-                    upper = mid - 1;
-                    continue;
-                }
-
-                // Else the element can only be present 
-                // in upper sub-array 
-                lower = mid + 1;
-            }
-        }
-
-        public static int BinarySearch(
-            this NativeArray<PrefabComponent> array,
-            int type
-        ) {
-            return array.BinarySearch(
-                new PrefabComponent((PrefabTypes)type, Entity.Null)
-            );
         }
     }
 }
